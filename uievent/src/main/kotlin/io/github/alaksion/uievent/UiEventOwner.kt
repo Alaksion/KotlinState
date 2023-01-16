e49e4a979d8e82f04af4f00dec2d639687adfa57
lucca <lbeurmann.dev@gmail.com>
2023-01-16 16:57:06 -0300

    docs: add docs to Ui event

--- uievent/src/main/kotlin/io/github/alaksion/uievent/UiEventOwner.kt
+++ uievent/src/main/kotlin/io/github/alaksion/uievent/UiEventOwner.kt
@@ -3,16 +3,48 @@ package io.github.alaksion.uievent
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.update
 
+/**
+ * Base Contract of an UiEventOwner, a class that manages a queue of UiEvents and enables consumer
+ * classes to collect and consume this events.
+ * */
 public interface UiEventOwner<T : UiEvent> {
 
+    /**
+     * StateFlow containing the current queue of UiEvents.
+     * */
     val eventQueue: StateFlow<List<T>>
 
+    /**
+     * Adds an event to the UiEvent queue.
+     * */
     fun sendEvent(event: T)
 
+    /**
+     * Consumes an event from the UiEvent queue, removing it from the current queue.
+     * */
     suspend fun consumeEvent(event: T)
 
+}
+
+/***
+ * Convenience function to collect the event queue StateFlow and execute a callback for every
+ * event received. Events collected by this function will be automatically disposed through the call
+ * of [UiEventOwner.consumeEvent].
+ *
+ * @param onEventReceived Callback to be executed when a new event is added to the queue
+ */
+suspend fun <T : UiEvent> UiEventOwner<T>.receiveEvents(
+    onEventReceived: (T) -> Unit
+) {
+    eventQueue.collectLatest { queue ->
+        queue.firstOrNull()?.let { event ->
+            onEventReceived(event)
+            consumeEvent(event)
+        }
+    }
 }
 
 public class UiEventHandler<T : UiEvent> : UiEventOwner<T> {
@@ -20,6 +52,7 @@ public class UiEventHandler<T : UiEvent> : UiEventOwner<T> {
     private val mutableEventQueue = MutableStateFlow(listOf<T>())
 
     override val eventQueue: StateFlow<List<T>> = mutableEventQueue.asStateFlow()
+
 
     override suspend fun consumeEvent(event: T) {
         mutableEventQueue.update { oldSnapshot ->
