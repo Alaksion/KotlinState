6cd41cf73e485d1186f5d3a8b1ee526b837eb41c
Lucca Beurmann <lbeurmann.dev@gmail.com>
2023-01-15 21:24:32 -0300

    add ui event (#3)

---
+++ uievent/src/main/kotlin/br/com/alaksion/uievent/UiEventOwner.kt
@@ -0,0 +1,38 @@
+package br.com.alaksion.uievent
+
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+public interface UiEventOwner<T : UiEvent> {
+
+    val eventQueue: StateFlow<List<T>>
+
+    fun sendEvent(event: T)
+
+    suspend fun consumeEvent(event: T)
+
+}
+
+public class UiEventHandler<T : UiEvent> : UiEventOwner<T> {
+
+    private val mutableEventQueue = MutableStateFlow(listOf<T>())
+
+    override val eventQueue: StateFlow<List<T>> = mutableEventQueue.asStateFlow()
+
+    override suspend fun consumeEvent(event: T) {
+        mutableEventQueue.update { oldSnapshot ->
+            oldSnapshot.filter { currentEvent ->
+                currentEvent.id != event.id
+            }
+        }
+    }
+
+    override fun sendEvent(event: T) {
+        mutableEventQueue.update { oldSnapshot ->
+            oldSnapshot + event
+        }
+    }
+
+}
